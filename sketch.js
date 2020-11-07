3e8c19a6e5119cd035848515758b4fa37fadba19
camihall <camihall@bu.edu>
2020-11-07 15:21:07 -0500

    I'm trying to reduce the white space in the ombre. By editing values in fill

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(1000, 600);
 }
 function draw() {
     noStroke();
-    fill(mouseX, mouseX-100, mouseY);
+    fill(mouseX, mouseX-mouseY, mouseY);
     if(mouseIsPressed){
       ellipse(mouseX, mouseY, 50, 50);
     }
